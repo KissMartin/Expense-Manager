4aef06f6d23c281d22be41db83cad2af6f1642bc
KissMartin <kiss.martin2@students.jedlik.eu>
2023-03-24 18:57:48 +0100

    modified layout, buttons added

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 
-class MyTabView(customtkinter.CTkTabview):  # pylint: disable=abstract-method
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-
-        self.add("tab 1")
-        self.add("tab 2")
-
-        self.label = customtkinter.CTkLabel(master=self.tab("tab 1"))
-        self.label.grid(row=0, column=0, padx=20, pady=10)
-
-
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -22,8 +11,22 @@ class App(customtkinter.CTk):
         self.title("Expense Operator")
         self.geometry(f"{1400}x{800}")
 
-        self.tab_view = MyTabView(master=self)
-        self.tab_view.grid(row=0, column=0, padx=20, pady=20)
+        self.sidebar_frame = SidebarFrame(master=self)
+        self.sidebar_frame.grid(row=0, column=0)
+
+
+class SidebarFrame(customtkinter.CTkFrame):  # pylint: disable=abstract-method
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.logo_label = customtkinter.CTkLabel(self, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.sidebar_button_1 = customtkinter.CTkButton(self, text='asd')
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_button_2 = customtkinter.CTkButton(self)
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_button_3 = customtkinter.CTkButton(self)
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=(10, 1000))
 
 
 if __name__ == "__main__":
