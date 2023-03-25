a6e57ea850e798300c87aae115c01cd8b8604a0c
KissMartin <kiss.martin2@students.jedlik.eu>
2023-03-25 13:11:45 +0100

    added example for easier work, managed to create the home page

--- main.py
+++ main.py
@@ -11,22 +11,38 @@ class App(customtkinter.CTk):
         self.title("Expense Operator")
         self.geometry(f"{1400}x{800}")
 
-        self.sidebar_frame = SidebarFrame(master=self)
-        self.sidebar_frame.grid(row=0, column=0)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.nav_frame = NavFrame(self, corner_radius=0)
+        self.nav_frame.grid(row=0, column=0, sticky="nsew")
+        self.nav_frame.grid_rowconfigure(4, weight=1)
+
+        self.settings_menu = Settings(master=self)
+        self.settings_button = customtkinter.CTkButton(self, text="Settings", command=None)
 
 
-class SidebarFrame(customtkinter.CTkFrame):  # pylint: disable=abstract-method
+class Settings(customtkinter.CTkEntry):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-        self.logo_label = customtkinter.CTkLabel(self, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self, text='asd')
-        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self)
-        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_3 = customtkinter.CTkButton(self)
-        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=(10, 1000))
+        self.entry = customtkinter.CTkEntry(master=self, placeholder_text="CTkEntry")
+
+
+class NavFrame(customtkinter.CTkFrame):  # pylint: disable=abstract-method
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.logo_label = customtkinter.CTkLabel(self, text="Expense Operator", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
+        self.expenses = customtkinter.CTkButton(self, text='Expenses')
+        self.expenses.grid(row=1, column=0, padx=20, pady=10, sticky="ew")
+        self.charts = customtkinter.CTkButton(self, text='Charts')
+        self.charts.grid(row=2, column=0, padx=20, pady=10, sticky="ew")
+        self.settings_button = customtkinter.CTkButton(self, text='Settings')
+        self.settings_button.grid(row=3, column=0, padx=20, pady=10, sticky="ew")
+        self.logged_in_as = customtkinter.CTkLabel(self, text='Logged in as: ')
+        self.logged_in_as.grid(row=4, column=0, padx=20, pady=10, sticky="sw")
 
 
 if __name__ == "__main__":
